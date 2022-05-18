0b311f2c60fbab22514facd973234d12587c3f25
vanquiche <stevevang2010@hotmail.com>
2022-05-18 13:21:36 +0900

    add custom fonts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { IconButton } from 'react-native-paper';
+import { useState, useEffect } from 'react';
+import { ActivityIndicator } from 'react-native';
+import { configureFonts, IconButton } from 'react-native-paper';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider as PaperProvider, DefaultTheme } from 'react-native-paper';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+
+import * as Font from 'expo-font';
 
 import 'react-native-gesture-handler';
 
@@ -14,17 +16,65 @@ import CategoryScreen from './screens/CategoryScreen';
 import ShopScreen from './screens/ShopScreen';
 import ProfileScreen from './screens/ProfileScreen';
 
+declare global {
+    namespace ReactNativePaper {
+
+    }
+}
+
 const Tab = createMaterialBottomTabNavigator();
 const queryClient = new QueryClient();
 
+const customFonts = {
+  BalooBhaiRegular: require('./assets/fonts/BalooBhai2-Regular.ttf'),
+  BalooBhaiMedium: require('./assets/fonts/BalooBhai2-Medium.ttf'),
+  BalooBhaiBold: require('./assets/fonts/BalooBhai2-Bold.ttf'),
+  BalooBhaiSemiBold: require('./assets/fonts/BalooBhai2-SemiBold.ttf'),
+  BalooBhaiExtraBold: require('./assets/fonts/BalooBhai2-ExtraBold.ttf'),
+};
+
 export default function App() {
+  const [loading, setLoading] = useState(true);
+
+
   const theme = {
     ...DefaultTheme,
+    roundness: 10,
     colors: {
       ...DefaultTheme.colors,
-      primary: '#8ecae6',
+      primary: '#FFD9D9',
+    },
+    fonts: {
+      ...DefaultTheme.fonts,
+      // regular: {
+      //   fontFamily: 'BalooBhaiRegular',
+      // },
+      thin: {
+        fontFamily: 'BalooBhaiMedium',
+      },
+      light: {
+        fontFamily: 'BalooBhaiSemiBold',
+      },
+      regular: {
+        fontFamily: 'BalooBhaiBold',
+      },
+      medium: {
+        fontFamily: 'BalooBhaiExtraBold',
+      },
     },
   };
+
+  useEffect(() => {
+    const loadFonts = async () => {
+      await Font.loadAsync(customFonts);
+      setLoading(false);
+    };
+    loadFonts();
+  }, []);
+
+  if (loading) {
+    return <ActivityIndicator size='large' />;
+  }
 
   return (
     <QueryClientProvider client={queryClient}>
@@ -43,7 +93,7 @@ export default function App() {
               name='Home-page'
               component={HomeScreen}
               options={{
-                tabBarIcon: ({focused}) => (
+                tabBarIcon: ({ focused }) => (
                   <IconButton
                     icon='home'
                     size={26}
@@ -57,7 +107,7 @@ export default function App() {
               name='Topics'
               component={CategoryScreen}
               options={{
-                tabBarIcon: ({focused}) => (
+                tabBarIcon: ({ focused }) => (
                   <IconButton
                     icon='cards'
                     size={26}
@@ -71,7 +121,7 @@ export default function App() {
               name='Shop-page'
               component={ShopScreen}
               options={{
-                tabBarIcon: ({focused}) => (
+                tabBarIcon: ({ focused }) => (
                   <IconButton
                     icon='store'
                     size={26}
@@ -85,7 +135,7 @@ export default function App() {
               name='Profile-page'
               component={ProfileScreen}
               options={{
-                tabBarIcon: ({focused}) => (
+                tabBarIcon: ({ focused }) => (
                   <IconButton
                     icon='heart'
                     size={26}
