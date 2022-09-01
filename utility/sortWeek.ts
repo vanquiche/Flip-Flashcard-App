2fba56068bfaedcc0f27daa399f6bc46130370ab
vanquiche <stevevang2010@hotmail.com>
2022-09-01 15:10:34 +0900

    update sortWeek to return new array on start of new week

--- utility/sortWeek.ts
+++ utility/sortWeek.ts
@@ -10,13 +10,15 @@ const sortWeek = (week: string[]) => {
     const { days } = dt.now().diff(dt.fromISO(w), 'days').toObject();
     return days && days >= 7 ? true : false;
   });
+  const startOfNewWeek = today === 7
 
   if (loginDay === today) {
     // if same day then do nothing
     return week;
-  } else if (week.length > 6 || weekExpired) {
+  } else if (week.length > 6 || weekExpired || startOfNewWeek) {
     // if login days are not in same week
     // if week have exceeded 7 logins
+    // if new week has started i.e. Sunday 
     return [dt.now().toISO()];
   } else return week.concat(dt.now().toISO())
 };
