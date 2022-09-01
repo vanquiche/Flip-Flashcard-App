23b0bde426e2d365a41e7ff911900ccf64222da0
vanquiche <stevevang2010@hotmail.com>
2022-09-01 17:52:49 +0900

    deploy build 12
    - add babel tranform remove console
    
    -  update expo-update package

--- babel.config.js
+++ babel.config.js
@@ -5,7 +5,10 @@ module.exports = function (api) {
     plugins: ['react-native-reanimated/plugin'],
     env: {
       production: {
-        plugins: ['react-native-paper/babel'],
+        plugins: [
+          'react-native-paper/babel',
+          ['transform-remove-console', { exclude: ['error', 'warn'] }],
+        ],
       },
     },
   };
